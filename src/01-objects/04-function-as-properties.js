0dbf115d8f9ff29d57bfef7bec3a3385c14b1fcc
niravkpatel28 <niravkpatel28@gmail.com>
2022-02-24 16:41:39 +0530

    Classes and Destructuring

--- src/01-objects/04-function-as-properties.js
+++ src/01-objects/04-function-as-properties.js
@@ -1,33 +1,34 @@
+// function Employee(name, company) {
+//   console.log("This is the constructor ");
+//   this.name = name;
+//   this.company = company;
+//   // Method/ functions are objects
+//   // method has to be shared ( part of function )
+//   this.printName = function () {
+//     console.log("This method is called");
+//     console.log("Welcome  ", this.name);
+//   };
+// }
+
 function Employee(name, company) {
   this.name = name;
   this.company = company;
-  this.printName = function () {
+  this.mainCompany = "Surfboard";
+  // Method/ functions are objects
+  // method has to be shared ( part of function )
+  this.__proto__.printName = function () {
+    console.log("This method is called");
     console.log("Welcome  ", this.name);
   };
+  this.__proto__.parentCompany = "Surfboard";
 }
+
+Employee.prototype.random = "Random Variable";
 
 let emp1 = new Employee("Anita", "Contentstack");
 let emp2 = new Employee("Roop", "Contentstack");
+let emp3 = new Employee("Nirav", "Surfboard");
+let emp4 = new Employee("Rahul", "Surfboard");
 
-// function Employee(name, company) {
-//   this.name = name;
-//   this.company = company;
-//   //   return this; // implicit when you use new
-// }
-
-// function printName() {
-//   console.log("Welcome  ", this.name);
-// }
-// const emp1 = {
-//   name: "Anita",
-//   company: "Contentstack",
-//   printName: printName,
-// };
-
-// const emp2 = {
-//   name: "Roop",
-//   company: "Contentstack",
-//   printName: printName,
-// };
-
-//
+// how many mainCompany = 4
+// how many parentCompany = 1
