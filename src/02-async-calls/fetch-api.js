29307428517d011ff0735c4bac8747ad84442b9e
Sayali <sayalijoshi27>
2022-03-14 12:56:32 +0530

    fetch json data and build html

---
+++ src/02-async-calls/fetch-api.js
@@ -0,0 +1,41 @@
+const url = 'https://jsonplaceholder.typicode.com/users'
+let element = document.getElementById('authors')
+
+const fetchAuthors = () => {
+fetch(url)
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    let authors = data
+    authors.map((author) => {
+      const {
+        address,
+        company,
+        email,
+        name, 
+        phone,
+        website
+      } = author
+      const { city, street, suite, zipcode } = address
+      element.innerHTML += `<li>
+        <div>
+          <h3>${name}</h3>
+          <h4>${company.name}</h4>
+          <address>
+            <p>${street}, ${suite}</p>
+            <p>${city}, ${zipcode}</p>
+          </address>
+          <p>${phone}</p>
+          <p><a href=${email} target="_blank">${email}</a></p>          
+          <p><a href=${website} target="_blank">${website}</a></p>
+        </div>
+      </li>`;
+    })    
+  }).catch((error) => {
+    console.log('Error', error)
+  });
+}
+fetchAuthors()
+  
+
